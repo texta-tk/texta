5d9edac2ce5e1233eb95cb5ced89db1295179f9d
Raul Sirel <rsirel@ut.ee>
2016-07-13 14:01:49 +0300

    full path for database file (needed in production mode)

--- settings.py
+++ settings.py
@@ -35,7 +35,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
     # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'lex.db',  # Or path to database file if using sqlite3.
+        'NAME': os.path.join(BASE_DIR,'lex.db'),  # Or path to database file if using sqlite3.
         'USER': '',  # Not used with sqlite3.
         'PASSWORD': '',  # Not used with sqlite3.
         'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
