4e91a878e77a548a2a5712da85c01e835bc3dfb5
Karl-Oskar Masing <komasing@gmail.com>
2017-11-20 15:01:54 +0200

    added sqlite version management to migrate.py

--- texta/settings.py
+++ texta/settings.py
@@ -144,13 +144,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
     # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR,'lex.db'),  # Or path to database file if using sqlite3.
+        'NAME': os.path.join(BASE_DIR, 'database', 'lex.db'),  # Or path to database file if using sqlite3.
         'USER': '',  # Not used with sqlite3.
         'PASSWORD': '',  # Not used with sqlite3.
         'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',  # Set to empty string for default. Not used with sqlite3.
+        'BACKUP_COUNT': 5,
     }
 }
+
+if not os.path.exists(os.path.dirname(DATABASES['default']['NAME'])):
+    os.makedirs(os.path.dirname(DATABASES['default']['NAME']))
 
 TIME_ZONE = 'Europe/Tallinn'
 LANGUAGE_CODE = 'et'
