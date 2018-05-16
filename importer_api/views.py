37d7af6b04142a84b7561175881246a11fa5d2e8
Marko Kollo <mrxkollo@users.noreply.github.com>
2018-05-16 16:11:58 +0300

    Locally working version, test setup and settings edit

--- importer_api/views.py
+++ importer_api/views.py
@@ -1,10 +1,11 @@
 from django.http import JsonResponse
 from account.models import Profile
 from django.views import View
-from elasticsearch.helpers import streaming_bulk as elastic_streambulk
-from texta.settings import es_url
+from elasticsearch.helpers import streaming_bulk as elastic_streambulk, bulk as elastic_bulk
+from texta.settings import es_url, INFO_LOGGER
 
 import json
+import logging
 import elasticsearch
 
 
@@ -16,18 +17,25 @@ class ElasticsearchHandler:
 		self.index = index
 		self.doc_type = doc_type
 
+		self.logger = logging.getLogger(INFO_LOGGER)
+
 	def insert_single_document(self, document):
-		self.es.index(index=self.index, doc_type=self.doc_type, body=document)
+		response = self.es.index(index=self.index, doc_type=self.doc_type, body=document)
+		self.logger.info(response)
 
 	def insert_multiple_documents(self, list_of_documents):
 		actions = [{"_source": document, "_index": self.index, "_type": self.doc_type} for document in list_of_documents]
-		elastic_streambulk(client=self.es, actions=actions, chunk_size=1000, max_retries=3)
+		for ok, response in elastic_streambulk(client=self.es, actions=actions, chunk_size=1000, max_retries=3):
+			if not ok:
+				self.logger.error(response)
 
 	def insert_index_into_es(self):
-		self.es.indices.create(index=self.index, ignore=400)
+		response = self.es.indices.create(index=self.index, ignore=400)
+		self.logger.info(response)
 
 	def insert_mapping_into_doctype(self, mapping_body):
-		self.es.indices.put_mapping(doc_type=self.doc_type, index=self.index, body=mapping_body)
+		response = self.es.indices.put_mapping(doc_type=self.doc_type, index=self.index, body=mapping_body)
+		self.logger.info(response)
 
 	def check_for_index_existance(self):
 		return self.es.indices.exists(index=self.index)
@@ -49,12 +57,12 @@ class ApiInputValidator:
 	def validate_field_existence(self):
 		for mandatory_field in self.mandatory_field_list:
 			if mandatory_field not in self.post_dict:
-				raise Exception("Mandatory field '{0}' is missing.".format(mandatory_field))
+				raise ValueError("Mandatory field '{0}' is missing.".format(mandatory_field))
 
 	def validate_value_existence(self):
 		for mandatory_field in self.mandatory_field_list:
 			if not self.post_dict[mandatory_field]:
-				raise Exception("Mandatory field '{0}' can not be empty".format(mandatory_field))
+				raise ValueError("Mandatory field '{0}' can not be empty".format(mandatory_field))
 
 
 class AuthTokenHandler:
@@ -66,22 +74,29 @@ class AuthTokenHandler:
 	def authenticate_token(self):
 		authenticated_token = Profile.objects.filter(auth_token=self.auth_token_str).first()
 		if not authenticated_token:
-			raise Exception("Authentication failed - invalid auth token.")
+			raise ValueError("Authentication failed - invalid auth token.")
 
 
 # Create your views here.
 class ImporterApiView(View):
 
-	def post(self, *args, **kwargs):
+	def post(self, request, *args, **kwargs):
 		try:
+			logger = logging.getLogger(INFO_LOGGER)
 
 			# In Python 3.5, json.loads() accepts only unicode, unlike in Python 3.6.
-			utf8_post_payload = self.request.body.decode("utf-8")
+			utf8_post_payload = request.body.decode("utf-8")
 			post_payload_dict = json.loads(utf8_post_payload)
 
+			logger.info("Validating mandatory field and value existances in ImportAPI")
 			ApiInputValidator(post_payload_dict, ["auth_token", "index", "doc_type", "data"])
+
+			logger.info("Authenticating user for ImportAPI.")
 			AuthTokenHandler(post_payload_dict.get("auth_token"))
+
+			logger.info("Creating ES client.")
 			es_handler = ElasticsearchHandler(index=post_payload_dict["index"], doc_type=post_payload_dict["doc_type"])
+			logger.info("Finished creating ES client.")
 
 			api_payload = post_payload_dict["data"]
 			es_handler.create_index_if_not_exist()
@@ -97,5 +112,5 @@ class ImporterApiView(View):
 
 			return JsonResponse({"message": "Item(s) successfully saved."})
 
-		except Exception as e:
-			return JsonResponse({"message": e})
+		except ValueError as e:
+			return JsonResponse({"message": str(e)})
