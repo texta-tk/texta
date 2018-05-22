18f839845c060ad2a7d7000d2dcad05dc1dd1433
Marit <marit.asula@gmail.com>
2018-05-22 12:07:21 +0300

    updated date_converter

--- dataset_importer/document_preprocessor/preprocessors/date_converter.py
+++ dataset_importer/document_preprocessor/preprocessors/date_converter.py
@@ -1,71 +1,84 @@
 # -*- coding: utf-8 -*-
 
-import dateutil.parser
+import dateparser
 import re
+#from dateparser.search import search_dates
+
 
 class DatePreprocessor(object):
     """
     Converts date fields to suitable format for ElasticSearch.
     """
-
+    
     def __init__(self):
-        self._months = {'jaanuar':'January',
-                  'veebruar':'February',
-                  'märts':'March',
-                  'aprill':'April',
-                  'mai':'May',
-                  'juuni':'June',
-                  'juuli':'July',
-                  'august':'August',
-                  'september':'September',
-                  'oktoober':'October',
-                  'november':'November',
-                  'detsember':'December'}
-                  
-        self._est_months = '|'.join(self._months.keys())
+        self._languages = ['et','en','ru']
+        self._date_pattern = self._get_date_patterns()
         
-        self._est_month_pattern = re.compile(self._est_months)
-        self._date_pattern = re.compile('\d{4}-\d{2}-\d{2}')
+    def _get_date_patterns(self):
         
-        def _convert_month(self,date_val):
-          '''
-          Converts month names from estonian to english
-          '''
-          try:
-              date_val = date_val.lower()
-              est_month_matches = self._est_month_pattern.findall(date_val)
-              for est_month in est_month_matches:
-                date_val = re.sub(est_month,self._months[est_month],date_val)
-          except Exception as e:
-              print(e)
-          return date_val
-          
-        def convert_date(self,date_field_value, **kwargs):
-          '''Converts given date field value to standard ES format yyyy-mm-dd
-          
-          :param date_field_value: date field value to convert
-          :param kwargs: request parameters which must include entries for the preprocessors to work appropriately
-          :type date_field_value: string
-          :return: date converted to standard ES format
-          :rtype: string
-          '''
-          
-          # TODO: 
-          '''
-          if not kwargs.get('mlp_preprocessor_input_features', None):
-            kwargs['mlp_preprocessor_input_features'] = '["text"]'
-          '''
-          try:
-              d = self._convert_month(date_field_value)
-              d = dateutil.parser.parse(d)
-              d = str(d)
+        '''
+        Compiles common date patterns for date extraction
+        '''
+        dp_1 = '(?<=\D)\d{1,2}\.\s*\d{1,2}\.\s*\d{1,4}(?=$|\D)'  # 02.05.2018
+        dp_2 = '(?<=\D)\d{1,2}\.\s*[a-züõöä]+\s*\d{2,4}(?=$|\D)' # 2. mai 2018
+        dps = dp_1 + '|' + dp_2
+        pattern = re.compile(dps)
+        return pattern
+    
+    def set_languages(self,langs):
+        '''
+        Set default languages for parsing dates
+        '''
+        self._languages = langs
               
-              date_matches = self._date_pattern.findall(d)
-              date_match = date_matches[0] if date_matches else None
-          except Exception as e:
-              print(e)
-              date_match = None
-          return date_match
+    def convert_date(self,date_field_value,langs=[]):#, **kwargs):
+      '''Converts given date field value to standard ES format yyyy-mm-dd
+      
+      :param date_field_value: date field value to convert
+      :param kwargs: request parameters which must include entries for the 
+                     preprocessors to work appropriately
+      :type date_field_value: string
+      :return: date converted to standard ES format
+      :rtype: string
+      '''
+      
+      # TODO: 
+      '''
+      if not kwargs.get('mlp_preprocessor_input_features', None):
+        kwargs['mlp_preprocessor_input_features'] = '["text"]'
+      '''
+      
+      if langs:
+          self._languages = langs
+      try:
+          datetime_object = dateparser.parse(date_field_value,languages=self._languages)
+          formatted_date = datetime_object.strftime('%Y-%m-%d')
 
+      except Exception as e:
+          print(e)
+          formatted_date = None
+      return formatted_date
+  
+    def extract_dates(self,text,convert=False):
+        dates = re.findall(self._date_pattern,text)
+        if convert:
+            dates = [self.convert_date(d) for d in dates]
+        return dates
+
+      
+
+'''
 if __name__ == '__main__':
     pass
+'''
+
+dp = DatePreprocessor()
+
+text = u' bla 03. juuni 2018 blablabla 02.03.2009 blabla'
+
+text_2 = '02.03.2009'
+dates = dp.extract_dates(text,convert=True)
+print dates
+
+
+
