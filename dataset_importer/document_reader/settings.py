67c542bea4a69354b42f65d86841b200ae1267ae
rsirel <rsirel@ut.ee>
2018-05-29 13:54:36 +0300

    change json->json lines

--- dataset_importer/document_reader/settings.py
+++ dataset_importer/document_reader/settings.py
@@ -96,7 +96,7 @@ except:
 
 try:
     collection_reader_map['json'] = {
-        'name': 'JSON',
+        'name': 'JSON lines',
         'parameter_tags': 'file',
         'class': readers.collection.json.JSONReader
     }
