a2a90f351b966b034bdcd540512eebb2d9adcefa
ranetp <ranetpuusepp@gmail.com>
2018-08-24 12:26:39 +0300

    fact manager fixes

--- searcher/fact_manager.py
+++ searcher/fact_manager.py
@@ -15,7 +15,8 @@ class FactManager:
         self.es_m = ES_Manager(self.dataset, self.mapping)
         self.field = 'texta_facts'
 
-    def remove_facts_from_document(self):
+    def remove_facts_from_document(self, bs=7500):
+        self.es_m.clear_readonly_block()
         query = {"main": {"query": {"nested":
             {"path": self.field,"query": {"bool": {"must":
             [{ "match": {self.field + ".fact": self.key }},
@@ -23,21 +24,24 @@ class FactManager:
             "_source": [self.field]}}
 
         self.es_m.load_combined_query(query)
-        response = self.es_m.scroll()
+        response = self.es_m.scroll(size=bs, field_scroll=self.field)
+        scroll_id = response['_scroll_id']
+        total_docs = response['hits']['total']
+        import pdb;pdb.set_trace()
+        while total_docs > 0:
+            data = ''
+            for document in response['hits']['hits']:
+                new_field = []
+                for fact in document['_source'][self.field]:
+                    if fact['fact'] != self.key and fact['str_val'] != self.val:
+                        new_field.append(fact)
 
-        data = ''
-        for document in response['hits']['hits']:
-            removed_facts = []
-            new_field = []
-            for fact in document['_source'][self.field]:
-                if fact['fact'] == self.key and fact['str_val'] == self.val:
-                    removed_facts.append(fact)
-                else:
-                    new_field.append(fact)
+                # Update dataset
+                data += json.dumps({"update": {"_id": document['_id'], "_type": document['_type'], "_index": document['_index']}})+'\n'
+                document = {'doc': {self.field: new_field}}
+                data += json.dumps(document)+'\n'
 
-            # Update dataset
-            data += json.dumps({"update": {"_id": document['_id'], "_type": document['_type'], "_index": document['_index']}})+'\n'
-            document = {'doc': {self.field: new_field}}
-            data += json.dumps(document)+'\n'
-
-        self.es_m.plain_post_bulk(self.es_m.es_url, data)
+            response = self.es_m.scroll(scroll_id=scroll_id, size=bs, field_scroll=self.field)
+            total_docs = len(response['hits']['hits'])
+            scroll_id = response['_scroll_id']
+            self.es_m.plain_post_bulk(self.es_m.es_url, data)
