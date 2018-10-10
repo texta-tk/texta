100687d45a63fdb8e645551ec9f8611a7a7d49e6
ranetp <ranetpuusepp@gmail.com>
2018-10-10 15:13:56 +0300

    Revert "Autocomplete fix"
    This reverts commit 208205e81035ec22e2b06ffbae07d9719baf4313.

--- utils/autocomplete.py
+++ utils/autocomplete.py
@@ -6,7 +6,7 @@ from utils.es_manager import ES_Manager
 class Autocomplete:
 
     def __init__(self):
-        self.es_m = None
+        self.es_m = None        
         self.lookup_type = None
         self.key_constraints = None
         self.content = None
@@ -14,11 +14,11 @@ class Autocomplete:
         self.limit = None
 
     def parse_request(self,request):
-
+    
         self.lookup_types = request.POST['lookup_types'].split(',')
         self.key_constraints = request.POST['key_constraints'].split(',')
         self.content = request.POST['content'].split('\n')[-1].strip()
-        print(self.content)
+        
         ds = Datasets().activate_dataset(request.session)
         self.dataset = ds.get_index()
         self.mapping = ds.get_mapping()
@@ -28,7 +28,7 @@ class Autocomplete:
 
     def suggest(self,limit=10):
         self.limit = limit
-
+        
         suggestions = {}
 
         for i,lookup_type in enumerate(self.lookup_types):
@@ -43,19 +43,30 @@ class Autocomplete:
         return suggestions
 
     def _get_facts(self, agg_subfield, lookup_type, key_constraint=None):
-        agg_query = {agg_subfield: {"nested": {"path": "texta_facts"}, "aggs": {agg_subfield: {"terms": {"field": "texta_facts.fact"}, "aggs": {"fact_values": {"terms": {"field": "texta_facts.str_val", "size": self.limit, "include": "{0}.*".format(self.content)}}}}}}}
+        agg_query = {
+                agg_subfield: {
+                    "nested": {"path": "texta_facts"},
+                    "aggs": {
+                        agg_subfield: {
+                            "terms": {"field": "texta_facts.{0}".format(agg_subfield), "size": self.limit, "include": "{0}.*".format(self.content)},
+                        }
+                    }
+                }
+            }
+        
+        agg_query = {agg_subfield: {"nested": {"path": "texta_facts"}, "aggs": {agg_subfield: {"terms": {"field": "texta_facts.fact"}, "aggs": {"fact_values": {"terms": {"field": "texta_facts.str_val"}}}}}}}
 
         self.es_m.build('')
         self.es_m.set_query_parameter("aggs", agg_query)
-
+        
         if lookup_type == 'FACT_VAL' and key_constraint:
-            facts = []
+            facts = []            
             for bucket in self.es_m.search()["aggregations"][agg_subfield][agg_subfield]["buckets"]:
                 if bucket["key"] == key_constraint:
                     facts += [self._format_suggestion(sub_bucket["key"], sub_bucket["key"]) for sub_bucket in bucket["fact_values"]["buckets"]]
-
+            
         elif lookup_type == 'FACT_VAL' and not key_constraint:
-            facts = []
+            facts = []          
             for bucket in self.es_m.search()["aggregations"][agg_subfield][agg_subfield]["buckets"]:
                 facts += [self._format_suggestion(sub_bucket["key"], sub_bucket["key"]) for sub_bucket in bucket["fact_values"]["buckets"]]
         else:
@@ -82,7 +93,7 @@ class Autocomplete:
 
                         suggestion = self._format_suggestion(concept.descriptive_term.term,display_text,resource_id=concept.pk)
                         concepts.append(suggestion)
-
+        
         return concepts
 
     def _get_lexicons(self):
@@ -96,7 +107,7 @@ class Autocomplete:
 
                 suggestion = self._format_suggestion(lexicon.name,display_text,resource_id=lexicon.pk)
                 suggested_lexicons.append(suggestion)
-
+                
         return suggested_lexicons
 
     @staticmethod
