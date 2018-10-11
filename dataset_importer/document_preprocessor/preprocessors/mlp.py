9e9071451ad4f99edf3ed702351199c29f9b29e3
ranetp <ranetpuusepp@gmail.com>
2018-10-11 16:15:34 +0300

    removed print

--- dataset_importer/document_preprocessor/preprocessors/mlp.py
+++ dataset_importer/document_preprocessor/preprocessors/mlp.py
@@ -20,15 +20,6 @@ class MlpPreprocessor(object):
         :type enabled_features: list of strings
         """
         self._mlp_url = mlp_url
-        print(mlp_url)
-        print(mlp_url)
-        print(mlp_url)
-        print(mlp_url)
-        print(mlp_url)
-        print(mlp_url)
-        print(mlp_url)
-        print(mlp_url)
-        print(mlp_url)
         self._enabled_features = set(enabled_features)
 
     def transform(self, documents, **kwargs):
