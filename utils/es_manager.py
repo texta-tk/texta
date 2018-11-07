1e6c563c9a4da9e5c634e2e6626be8adca566c07
Raul Sirel <rsirel@texta.ee>
2018-11-07 16:13:13 +0200

    multi field support in gui

--- utils/es_manager.py
+++ utils/es_manager.py
@@ -1,27 +1,15 @@
 # -*- coding: utf8 -*-
 from __future__ import print_function
 import json
-# import re
 import copy
 import requests
-# from collections import defaultdict
-# import sys
-# import time
 from functools import reduce
-
-# # Import django-stuff only if imported from the django application / prevent errors when importing from scripts
-# if 'django' in sys.modules:
-#     from conceptualiser.models import Concept
-#     from conceptualiser.models import TermConcept
-#     from utils.log_manager import LogManager
-#     from lexicon_miner.models import Word,Lexicon
 
 from utils.query_builder import QueryBuilder
 from texta.settings import es_url, es_use_ldap, es_ldap_user, es_ldap_password, FACT_PROPERTIES
 
 # Need to update index.max_inner_result_window to increase
 HEADERS = {'Content-Type': 'application/json'}
-
 
 class Singleton(type):
 
