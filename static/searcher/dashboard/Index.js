38117ac47c1dde651eb2e9c3f2651a0299d07e11
gpaimla <gertpaimla@gmail.com>
2019-01-17 15:20:45 +0200

    sigsterm, initial

--- static/searcher/dashboard/Index.js
+++ static/searcher/dashboard/Index.js
@@ -3,19 +3,68 @@ class Index {
         this.aggregations = aggregations;
         this.index_name = index_name;
         this.total_documents = total_documents;
+        this.AggregationTpes = {
+            "VALUECOUNT": 'valuecount',
+            "STERMS": 'sterms',
+            "SIGSTERMS": 'sigsterms',
+            "DATE_HISTOGRAM": 'date_histogram',
+            "NESTED": 'nested'
+        };
+        Object.freeze(this.AggregationTpes);
     }
 
     getDates() {
-        if(checkNested(this.aggregations, 'date_histogram','kuupaev_year','buckets')){
+        if (checkNested(this.aggregations, 'date_histogram', 'kuupaev_year', 'buckets')) {
             return this.aggregations.date_histogram.kuupaev_year.buckets;
-        }else{
+        } else {
             console.error(`index named ${this.index_name} does not have date_histogram field!`);
             return undefined
         }
-
     }
+
+
+    formatSTERMS(field) {
+        /* datatables parsable format */
+        let result = (this.aggregations[this.AggregationTpes.STERMS][field].buckets.map((e) => {
+            return [e.key, e.doc_count]
+        }));
+
+        /* filter out garbage */
+        let notAllowedToEnterHeaven = []
+        result.filter((e) => {
+            if (e[1] < 50) {
+                notAllowedToEnterHeaven.push(e[1])
+            }
+        });
+
+        if (notAllowedToEnterHeaven.length < 3 && result.length > 3) {
+            return result;
+        }
+        return null;
+    }
+    formatSIGSTERMS(field) {
+        /* datatables parsable format */
+        let result = (this.aggregations[this.AggregationTpes.SIGSTERMS][field].buckets.map((e) => {
+            return [e.key, e.doc_count]
+        }));
+
+        /* filter out garbage */
+        let notAllowedToEnterHeaven = []
+        result.filter((e) => {
+            if (e[1] < 50) {
+                notAllowedToEnterHeaven.push(e[1])
+            }
+        });
+
+        if (notAllowedToEnterHeaven.length < 3 && result.length > 3) {
+            return result;
+        }
+        return null;
+    }
+
 }
-function checkNested(obj ) {
+
+function checkNested(obj) {
     var args = Array.prototype.slice.call(arguments, 1);
 
     for (var i = 0; i < args.length; i++) {
--- static/searcher/dashboard/dashboard.js
+++ static/searcher/dashboard/dashboard.js
@@ -1,15 +1,14 @@
 var PREFIX = LINK_SEARCHER;
 
 
-
-$(function() {
+$(function () {
     console.log(PREFIX);
     $.get(PREFIX + '/dashboard', function (data) {
         let indices = [];
 
-        if(checkNested(data, 'indices')){
+        if (checkNested(data, 'indices')) {
             console.table(data.indices);
-            data.indices.forEach((element)=>{
+            data.indices.forEach((element) => {
                 indices.push(new Index(element.aggregations, element.index_name, element.total_documents))
             })
         }
@@ -17,74 +16,88 @@ $(function() {
         initDashBoard(indices)
     });
 });
-function initListeners(){
+
+function initListeners() {
     let previous = '';
-    $('#index_fields').on('change', function() {
+    $('#index_fields').on('change', function () {
         console.log(this.value);
-        if(previous === ''){
+        if (previous === '') {
             $(`#datatables-container-${this.value}`).removeClass('hidden');
             previous = this.value
-        }else{
+        } else {
             $(`#datatables-container-${previous}`).addClass('hidden');
             $(`#datatables-container-${this.value}`).removeClass('hidden');
             previous = this.value
         }
     });
 }
-function initDashBoard(indices){
+
+function initDashBoard(indices) {
     //timelines
-    indices.forEach((e)=>{
+    indices.forEach((e) => {
         makeTimeline(e);
-        makeTables(e)
+        makeSTERMSTables(e);
+        makeSIGSTERMSTables(e);
     })
 }
 
 function makeTimeline(index) {
-    let div = document.getElementById('timeline-agg-container-'+index.index_name);
+    let div = document.getElementById('timeline-agg-container-' + index.index_name);
     let dates = index.getDates();
 
-    if (dates){
-        let xData = dates.map((e)=> e.key_as_string);
-        let yData = dates.map((e)=> e.doc_count);
-        Plotly.plot( div, [{
+    if (dates) {
+        let xData = dates.map((e) => e.key_as_string);
+        let yData = dates.map((e) => e.doc_count);
+        Plotly.plot(div, [{
             x: xData,
-            y: yData }], {
-            margin: { t: 0 } } );
-    }else{
+            y: yData
+        }], {
+            margin: {t: 0}
+        });
+    } else {
         div.remove()
     }
 }
-function makeTables(index){
-    // todo: fix all of this, make it better
+function makeSIGSTERMSTables(index){
+    for (let field in index.aggregations[index.AggregationTpes.SIGSTERMS]) {
+        let result = index.formatSIGSTERMS(field)
 
-    for (let field in index.aggregations.sterms){
-        let columnName = field;
-        let sterms =( index.aggregations.sterms[field].buckets.map((e)=>{
-
-            return [e.key, e.doc_count]
-
-        }));
-        let notAllowedToEnterHeaven = []
-        sterms.filter((e)=>{
-            if(e[1] <50){
-                notAllowedToEnterHeaven.push(e[1])
-            }
-        })
-
-        if(sterms.length > 5 && notAllowedToEnterHeaven.length <3){
-            $('#datatables-container-'+index.index_name).append(`<table id="generated-${field}" style="width:100%"></table>`);
-            console.table(sterms);
-            $('#generated-'+field).DataTable( {
-                data: sterms,
+        if (result != null) {
+            $('#'+index.index_name+'-sigsterms-table' ).append(`<table id="${index.AggregationTpes.SIGSTERMS}-generated-${field}" style="width:100%"></table>`);
+            console.table(result);
+            $(`#${index.AggregationTpes.SIGSTERMS}-generated-${field}`).DataTable({
+                data: result,
                 dom: 't',
                 ordering: true,
-                order: [ 1, 'desc' ],
+                order: [1, 'desc'],
                 paging: false,
                 columns: [
-                    { title: columnName },
-                    { title: "Key" }
+                    {title: field},
+                    {title: "count"}
                 ]
-            } );
+            });
+        }
+    }
+}
+function makeSTERMSTables(index) {
+
+    for (let field in index.aggregations[index.AggregationTpes.STERMS]) {
+        let result = index.formatSTERMS(field)
+
+        if (result != null) {
+            $('#'+index.index_name+'-sterms-table' ).append(`<table id="${index.AggregationTpes.STERMS}-generated-${field}" style="width:100%"></table>`);
+            console.table(result);
+            $(`#${index.AggregationTpes.STERMS}-generated-${field}`).DataTable({
+                data: result,
+                dom: 't',
+                ordering: true,
+                order: [1, 'desc'],
+                paging: false,
+                columns: [
+                    {title: field},
+                    {title: "count"}
+                ]
+            });
         }
     }
 }
