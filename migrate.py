0ebeb87aaaaeb88573c3992ef2593949c701d2d3
Raul Sirel <rsirel@texta.ee>
2019-01-18 09:56:02 +0200

    updated migrate.py

--- migrate.py
+++ migrate.py
@@ -1,23 +1,14 @@
 import django # For making sure the correct Python environment is used.
-from texta.settings import INSTALLED_APPS, DATABASES
+from texta.settings import INSTALLED_APPS
 import subprocess
 from time import sleep
-import shutil
 import sys
 import os
-import time
-import datetime
-import shutil
-import glob
-import re
-import operator
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "texta.settings")
 django.setup()
 from django.contrib.auth.models import User
-
-BACKUP_STRFTIME_FORMAT = '%Y-%m-%d %H-%M-%S'
-BACKUP_DATE_PATTERN = re.compile('pre-(\d{4}-\d{2}-\d{2} \d{2}-\d{2}-\d{2})')
 
 
 def create_admin():
@@ -33,10 +24,20 @@ def create_admin():
         return False
 
 
+def migrate(custom_apps):
+    print('Toolkit: Detecting database changes.')
+    make_migrations_output = subprocess.check_output(['python', 'manage.py', 'makemigrations'] + custom_apps)
+    print('Toolkit: Making database changes.')
+    sleep(2)
+    migrate_output = subprocess.check_output(['python', 'manage.py', 'migrate'])
+    print('Toolkit: Creating Admin user if necessary.')
+    sleep(2)
+    create_admin()
+    return True
+
+
 cwd = os.path.realpath(os.path.dirname(__file__))
-
 custom_apps = [app for app in INSTALLED_APPS if not app.startswith('django')] # Migration works for custom apps only. Manage.py can't detect relevant built-in django apps.
-
 
 if len(sys.argv) > 1:
     if sys.argv[1] == 'purge_migrations':
@@ -46,23 +47,9 @@ if len(sys.argv) > 1:
             if os.path.exists(migrations_dir):
                 shutil.rmtree(migrations_dir)
 
-print('Detecting database changes...')
-print('')
-sleep(2)
-make_migrations_output = subprocess.check_output(['python', 'manage.py', 'makemigrations'] + custom_apps)
-print(make_migrations_output)
-print('')
-
-sleep(3)
-
-print('Making database changes...')
-print('')
-sleep(2)
-migrate_output = subprocess.check_output(['python', 'manage.py', 'migrate'])
-print(migrate_output)
-print('')
-
-print('Creating Admin user...')
-print ('')
-sleep(2)
-print(create_admin())
+try:
+    migrate(custom_apps)
+except Exception:
+    print('Migrations failed - MySQL possibly not ready yet. Sleeping for 10 sec & trying again.')
+    sleep(10)
+    migrate(custom_apps)
