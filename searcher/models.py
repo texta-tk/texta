0e646f3c234e43ee15148cca4ed2a79da48dd122
ranetp <ranetpuusepp@gmail.com>
2019-01-29 14:04:06 +0200

    added default null value to searcher dataset

--- searcher/models.py
+++ searcher/models.py
@@ -11,7 +11,7 @@ class Search(models.Model):
     id = models.AutoField(primary_key=True)
     search_content = models.TextField(default='') # JSON string
     description = models.CharField(max_length=MAX_STR_LEN)
-    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE) # NEW PY REQUIREMENT
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE , null=True) # NEW PY REQUIREMENT
     author = models.ForeignKey(User, on_delete=models.CASCADE) # NEW PY REQUIREMENT
     query = models.TextField(default='')
 
