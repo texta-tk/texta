c68b22a7a366aaa5f16ec221f75e7474c8db95fc
ranetp <ranetpuusepp@gmail.com>
2019-01-30 14:07:43 +0200

    added text tagger custom field path

--- task_manager/document_preprocessor/preprocessors/text_tagger.py
+++ task_manager/document_preprocessor/preprocessors/text_tagger.py
@@ -53,30 +53,29 @@ class TextTaggerPreprocessor(object):
                     text_map[field].append(decoded_text.strip())
 
         # Apply tags to every input feature
-        for field in input_features:
-            results = []
-            tagger_descriptions = []
+        results = []
+        tagger_descriptions = []
 
-            for tagger in taggers_to_apply:
-                tagger_descriptions.append(tagger.description)
-                result_vector = tagger.tag(text_map)
-                results.append(result_vector)
-            results_transposed = np.array(results).transpose()
+        for tagger in taggers_to_apply:
+            tagger_descriptions.append(tagger.description)
+            result_vector = tagger.tag(text_map)
+            results.append(result_vector)
+        results_transposed = np.array(results).transpose()
 
-            for i, tagger_ids in enumerate(results_transposed):
-                positive_tag_ids = np.nonzero(tagger_ids)
-                positive_tags = [tagger_descriptions[positive_tag_id] for positive_tag_id in positive_tag_ids[0]]
-                texta_facts = []
+        for i, tagger_ids in enumerate(results_transposed):
+            positive_tag_ids = np.nonzero(tagger_ids)
+            positive_tags = [tagger_descriptions[positive_tag_id] for positive_tag_id in positive_tag_ids[0]]
+            texta_facts = []
 
-                if positive_tags:
-                    if 'texta_facts' not in documents[i]:
-                        documents[i]['texta_facts'] = []
-                    for tag in positive_tags:
-                        new_fact = {'fact': 'TEXTA_TAG', 'str_val': tag, 'doc_path': field, 'spans': json.dumps([[0, len(text_map[field][i])]])}
-                        texta_facts.append(new_fact)
-                    documents[i]['texta_facts'].extend(texta_facts)
+            if positive_tags:
+                if 'texta_facts' not in documents[i]:
+                    documents[i]['texta_facts'] = []
+                for tag in positive_tags:
+                    new_fact = {'fact': 'TEXTA_TAG', 'str_val': tag, 'doc_path': input_path, 'spans': json.dumps([0, 0])}
+                    texta_facts.append(new_fact)
+                documents[i]['texta_facts'].extend(texta_facts)
 
-            # Get total tagged documents, get np array of results
-            total_positives = np.count_nonzero(results)
+        # Get total tagged documents, get np array of results
+        total_positives = np.count_nonzero(results)
 
         return {"documents":documents, "meta": {'documents_tagged': total_positives}}
