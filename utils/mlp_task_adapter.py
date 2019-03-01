dbce0de22bbf0e0ba8d044f7709da9f0a8561a29
rsirel <rsirel@texta.ee>
2019-03-01 16:23:37 +0200

    hotfix. strip leading / in mlp url

--- utils/mlp_task_adapter.py
+++ utils/mlp_task_adapter.py
@@ -5,7 +5,7 @@ import logging
 class MLPTaskAdapter(object):
 
     def __init__(self, mlp_url, mlp_type='mlp'):
-        self.start_task_url = '{0}/task/start/{1}'.format(mlp_url, mlp_type)
+        self.start_task_url = '{0}/task/start/{1}'.format(mlp_url.strip('/'), mlp_type)
     
     def process(self, data):
         errors = {}
