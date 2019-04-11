592015575ad8a76ce3c7afb056619fc3a3b16bae
ranetp <ranetpuusepp@gmail.com>
2019-04-11 13:31:00 +0300

    plot/result workaround

--- task_manager/tasks/workers/neuroclassifier/neuroclassifier_worker.py
+++ task_manager/tasks/workers/neuroclassifier/neuroclassifier_worker.py
@@ -56,7 +56,8 @@ class NeuroClassifierWorker(BaseWorker):
             validation_split {float} -- The percentage of data to use for validation
             crop_amount {int} -- If given, the amount to crop the training data. Useful for quick prototyping.
             grid_downsample_amount {float} -- The amount to downsample the grid search of the hyperparameters. Recommended less than 0.01 to avoid computation cost and time.
-            max_seq_len {int} -- The maximum sequence length given by the user. Used for deriving self.final_seq_len
+            seq_len {int} -- The sequence length for the model, can be limited by the given as max_seq_len param
+            vocab_size {int} -- The vocabulary size for the model, can be limited by the user as a max_vocab_size param
             num_epochs {int} -- In case of non-auto model, num_epochs will be the number of epochs to train the model for
         """
 
@@ -74,7 +75,6 @@ class NeuroClassifierWorker(BaseWorker):
         self.validation_split = None
         self.crop_amount = None
         self.grid_downsample_amount = None
-        self.max_seq_len = None
         self.num_epochs = None
         self.bs = 64
 
@@ -84,7 +84,7 @@ class NeuroClassifierWorker(BaseWorker):
 
         # Derived params
         self.vocab_size = None
-        self.final_seq_len = None
+        self.seq_len = None
 
         # Processed data
         self.X_train = None
@@ -95,6 +95,28 @@ class NeuroClassifierWorker(BaseWorker):
         self.model = None
 
 
+    def run(self, task_id):
+        self._set_up_task(task_id)
+
+        self._build_data_sampler()
+        self._process_data()
+        self._get_model()
+        self._train_model()
+        self._cross_validation()
+        self._create_task_result()
+        self._save_model()
+
+        self.show_progress.update(4)
+        self.task_obj.update_status(Task.STATUS_COMPLETED, set_time_completed=True)
+        # TODO get model weights/compile model
+        # TODO validate that auto models work
+        # TODO preprocessor
+
+        # conda install graphviz
+        # conda install keras/cuda/etc
+        # pip install git+https://github.com/autonomio/talos.git@daily-dev --upgrade
+
+
     def _set_up_task(self, task_id):
         self.task_id = task_id
         self.task_obj = Task.objects.get(pk=self.task_id)
@@ -103,7 +125,7 @@ class NeuroClassifierWorker(BaseWorker):
         self.model_name = 'model_{0}'.format(self.task_obj.unique_id)
 
         self.model_arch = self.task_params['model_arch']
-        self.max_seq_len = int(self.task_params['max_seq_len'])
+        self.seq_len = int(self.task_params['max_seq_len'])
         self.vocab_size = int(self.task_params['max_vocab_size'])
         self.num_epochs = int(self.task_params['num_epochs'])
         self.crop_amount = int(self.task_params['crop_amount'])
@@ -144,101 +166,6 @@ class NeuroClassifierWorker(BaseWorker):
         self.labels = data_sample_y
 
 
-    def _train_model(self):
-        self.show_progress.update(2)
-        if self.model_is_auto:
-            self._train_auto_model()
-        else:
-            history = self._train_generic_model()
-        return history
-
-
-    def run(self, task_id):
-        self._set_up_task(task_id)
-
-        self._build_data_sampler()
-        self._process_data()
-        self._get_model()
-        training_history = self._train_model()
-        loss_plot_url, acc_plot_url, model_plot_url = self._plot_model_and_history(training_history)
-        val_eval, train_eval = self._cross_validation()
-        self._create_task_result(val_eval, train_eval, loss_plot_url, acc_plot_url, model_plot_url)
-        self._save_model()
-
-        self.show_progress.update(4)
-        self.task_obj.update_status(Task.STATUS_COMPLETED, set_time_completed=True)
-        # TODO preprocessor
-        # TODO verbose overview
-        # TODO validate that auto models work
-
-        # TODO handle history of auto model
-        # TODO uncessessary passing of params now that task result is in self
-
-        # ?TODO import speed overview
-        # ?TODO average/percentile seq len
-        # ?TODO pretrained embedding
-        # validation result into results #DONE
-        # final seq/vocab size into results #DONE
-        # num_epocs param #DONE
-        # model summary to front #DONE
-        # debug why val results are random
-        # fix max sample size per class 
-        # conda install graphviz
-        # conda install keras/cuda/etc
-
-
-    def _create_task_result(self, val_eval, train_eval, loss_plot_url, acc_plot_url, model_plot_url):
-        train_summary = {
-            'X_train.shape': self.X_train.shape,
-            'y_train.shape': self.y_train.shape,
-            'X_val.shape': self.X_val.shape,
-            'y_val.shape': self.y_val.shape,
-            'final_seq_len': self.final_seq_len,
-            'vocab_size': self.vocab_size,
-            'val_acc': "{0:.4f}".format(val_eval[1]),
-            'val_loss': "{0:.4f}".format(val_eval[0]),
-            'train_acc': "{0:.4f}".format(train_eval[1]),
-            'train_loss': "{0:.4f}".format(train_eval[0]),
-            'tokenizer_num_words': len(self.tokenizer.word_index),
-            # NOTE Save plots as HTML images for now, whilst there is no better alternative
-            'acc_plot':  '<img src="{}" style="max-width: 80%">'.format(acc_plot_url),
-            'loss_plot':  '<img src="{}" style="max-width: 80%">'.format(loss_plot_url),
-            'model_plot':  '<img src="{}" style="max-width: 80%">'.format(model_plot_url),
-        }
-
-        self.task_result.update(train_summary)
-        self.task_obj.result = json.dumps(self.task_result)
-
-
-    def _cross_validation(self):
-        # Evaluate model, get [loss, accuracy]
-        val_eval = self.model.evaluate(x=self.X_val, y=self.y_val, batch_size=self.bs, verbose=1)
-        train_eval = self.model.evaluate(x=self.X_train, y=self.y_train, batch_size=self.bs, verbose=1)
-        return val_eval, train_eval
-
-
-    def _save_model(self):
-        try:
-            self.show_progress.update(3)
-            # create_file_path from helper_functions creates missing folders and returns a path
-            output_model_file = create_file_path(self.model_name, MODELS_DIR, self.task_type)
-            output_tokenizer_file = create_file_path('{}_{}'.format(self.model_name, 'tokenizer'), MODELS_DIR, self.task_type)
-            self.model.save(output_model_file)
-            with open(output_tokenizer_file, 'wb') as handle:
-                pickle.dump(self.tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        except Exception as e:
-            logging.getLogger(ERROR_LOGGER).error('Failed to save NeuroClassifier model to the filesystem.', exc_info=True, extra={
-                'model_name': self.model_name,
-                'file_path':  output_model_file
-            })
-
-
-    def _get_model(self):
-        model_obj = NeuroModels().get_model(self.model_arch)
-        self.model_is_auto = model_obj['auto']
-        self.model = model_obj['model']
-
-
     def _process_data(self):
         self.show_progress.update(1)
         # Declare Keras Tokenizer
@@ -253,12 +180,12 @@ class NeuroClassifierWorker(BaseWorker):
 
         # Get the max length of sequence of X_train values
         uncropped_max_len = max((len(x) for x in self.X_train))
-        # Set the final seq_len to be either the max_seq_len or the max unpadded/cropped in present in the dataset
+        # Set the final seq_len to be either the user set seq_len or the max unpadded/cropped in present in the dataset
         # TODO in the future, use values such as "average length" or "top X-th percentile length" for more optimal seq_len
-        self.final_seq_len = min(self.max_seq_len, uncropped_max_len)
+        self.seq_len = min(self.seq_len, uncropped_max_len)
 
-        # Pad sequence to match MAX_SEQ_LEN
-        self.X_train = pad_sequences(self.X_train, maxlen=self.final_seq_len)
+        # Pad sequence to match max seq_len
+        self.X_train = pad_sequences(self.X_train, maxlen=self.seq_len)
 
         # Split data, so it would be shuffeled before cropping
         self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(self.X_train, self.labels, test_size=self.validation_split, random_state=42)
@@ -275,10 +202,26 @@ class NeuroClassifierWorker(BaseWorker):
             self.vocab_size = final_vocab_size
 
 
+    def _train_model(self):
+        self.show_progress.update(2)
+        if self.model_is_auto:
+            scan = self._train_auto_model()
+            self._plot_auto_model(scan)
+        else:
+            history = self._train_generic_model()
+            self._plot_generic_model(history)
+
+
+    def _get_model(self):
+        model_obj = NeuroModels().get_model(self.model_arch)
+        self.model_is_auto = model_obj['auto']
+        self.model = model_obj['model']
+
+
     def _train_generic_model(self):
         # Custom cyclical learning rate callback
         clr_triangular = CyclicLR(mode='triangular', step_size=6*(len(self.X_train)/self.bs))
-        self.model = self.model(self.vocab_size, self.final_seq_len)
+        self.model = self.model(self.vocab_size, self.seq_len)
         history = self.model.fit(self.X_train, self.y_train,
                         batch_size=self.bs,
                         epochs=self.num_epochs,
@@ -304,12 +247,11 @@ class NeuroClassifierWorker(BaseWorker):
             'epochs': [2, 5],
             'dropout': (0, 0.20, 10),
             'optimizer': [Adam],
-            'seq_len': [self.final_seq_len],
+            'seq_len': [self.seq_len],
             'vocab_size': [self.vocab_size],
             'last_activation': [sigmoid],
             'shapes': ['brick', 'triangle', 'funnel']
         }
-
         scan_filename = "{}_scan.csv".format(self.model_name)
         scan_filepath = create_file_path(scan_filename, PROTECTED_MEDIA, "task_manager", self.task_type)
 
@@ -324,27 +266,82 @@ class NeuroClassifierWorker(BaseWorker):
             experiment_name=scan_filepath
         )
 
+        # Get only the model from the Talos experiment
+        best_model_index = best_model(scan, 'val_acc', False)
+        self.model = activate_model(scan, best_model_index)
+        # TODO get model weights/compile model
+        # Then use it to predict
+        # Then done with worker, move on to preprocessor
+        import pdb;pdb.set_trace()
+
+        return scan
+
+    def _create_task_result(self):
+        train_summary = {
+            'X_train.shape': self.X_train.shape,
+            'y_train.shape': self.y_train.shape,
+            'X_val.shape': self.X_val.shape,
+            'y_val.shape': self.y_val.shape,
+            'seq_len': self.seq_len,
+            'vocab_size': self.vocab_size,
+            'tokenizer_num_words': len(self.tokenizer.word_index),
+        }
+        self.task_result.update(train_summary)
+        self.task_obj.result = json.dumps(self.task_result)
+
+
+    def _cross_validation(self):
+        # Evaluate model, get [loss, accuracy]
+        val_eval = self.model.evaluate(x=self.X_val, y=self.y_val, batch_size=self.bs, verbose=1)
+        train_eval = self.model.evaluate(x=self.X_train, y=self.y_train, batch_size=self.bs, verbose=1)
+
+        self.task_result['val_acc'] = "{0:.4f}".format(val_eval[1]),
+        self.task_result['val_loss'] = "{0:.4f}".format(val_eval[0]),
+        self.task_result['train_acc'] = "{0:.4f}".format(train_eval[1]),
+        self.task_result['train_loss'] = "{0:.4f}".format(train_eval[0]),
+
+
+    def _save_model(self):
+        try:
+            self.show_progress.update(3)
+            # create_file_path from helper_functions creates missing folders and returns a path
+            output_model_file = create_file_path(self.model_name, MODELS_DIR, self.task_type)
+            output_tokenizer_file = create_file_path('{}_{}'.format(self.model_name, 'tokenizer'), MODELS_DIR, self.task_type)
+            self.model.save(output_model_file)
+            with open(output_tokenizer_file, 'wb') as handle:
+                pickle.dump(self.tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        except Exception as e:
+            logging.getLogger(ERROR_LOGGER).error('Failed to save NeuroClassifier model to the filesystem.', exc_info=True, extra={
+                'model_name': self.model_name,
+                'file_path':  output_model_file
+            })
+
+
+    def _plot_auto_model(self, scan):
         # Set results
         report = Reporting(scan)
         self.task_result['The number of rounds in the experiment'] = report.rounds()
         self.task_result['The number of rounds it took to get highest result'] = report.rounds2high()
+        self.task_result['Highest val_acc'] = report.high()
         self.task_result['The lowest val_acc'] = report.low()
-        self.task_result['The highest val_acc'] = report.high()
+        self.task_result['Highest train_acc'] = report.high('acc')
+        self.task_result['Lowest train_acc'] = report.low('acc')
+        self.task_result['Highest val_loss'] = report.high('val_loss')
+        self.task_result['Lowest val_loss'] = report.low('val_loss')
+        self.task_result['Highest train_loss'] = report.high('loss')
+        self.task_result['Lowest train_loss'] = report.low('loss')
+
 
         # Plot
         scan_plot_name = "{}_auto_acc.svg".format(self.model_name)
         scan_plot_path = create_file_path(scan_plot_name, PROTECTED_MEDIA, "task_manager", self.task_type)
-        scan_plot_url = os.path.join(URL_PREFIX, MEDIA_URL, "task_manager", self.task_type, acc_plot_name)
+        scan_plot_url = os.path.join(URL_PREFIX, MEDIA_URL, "task_manager", self.task_type, scan_plot_name)
         report.plot_line()
         plt.savefig(scan_plot_path, format="svg", bbox_inches='tight')
-        self.task_result['Auto model val_acc per round plot'] = '<img src="{}" style="max-width: 80%">'.format(scan_plot_url)
-
-        # Get only the model from the Talos experiment
-        best_model_index = best_model(scan, 'val_acc', False)
-        self.model = activate_model(scan, best_model_index)
+        self.task_result['Auto model val_acc per round'] = '<img src="{}" style="max-width: 80%">'.format(scan_plot_url)
 
 
-    def _plot_model_and_history(self, history):
+    def _plot_generic_model(self, history):
         # Plot training & validation accuracy values
         plt.plot(history.history['acc'])
         plt.plot(history.history['val_acc'])
@@ -377,4 +374,7 @@ class NeuroClassifierWorker(BaseWorker):
         model_plot_url = os.path.join(URL_PREFIX, MEDIA_URL, "task_manager", self.task_type, model_plot_name)
         plot_model(self.model, to_file=model_plot_path, show_shapes=True)
 
-        return loss_plot_url, acc_plot_url, model_plot_url
+        # NOTE Save plots as HTML images for now, whilst there is no better alternative
+        self.task_result['acc_plot'] = '<img src="{}" style="max-width: 80%">'.format(acc_plot_url),
+        self.task_result['loss_plot'] = '<img src="{}" style="max-width: 80%">'.format(loss_plot_url),
+        self.task_result['model_plot'] = '<img src="{}" style="max-width: 80%">'.format(model_plot_url),
