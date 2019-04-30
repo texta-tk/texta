140559202e9b71ed0265045d5b6be82f2caebdc4
ranetp <ranetpuusepp@gmail.com>
2019-04-30 08:30:34 +0300

    Add blank=True to datasets

--- searcher/models.py
+++ searcher/models.py
@@ -10,7 +10,7 @@ MAX_STR_LEN = 100
 class Search(models.Model):
     search_content = models.TextField(default='') # JSON string
     description = models.CharField(max_length=MAX_STR_LEN)
-    datasets = models.ManyToManyField(Dataset)
+    datasets = models.ManyToManyField(Dataset, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     query = models.TextField(default='')
 
