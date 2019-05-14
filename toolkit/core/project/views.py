5b2898124d179ac2d704a1def3a85cd2340704f6
unknown <ranetpuusepp@gmail.com>
2019-05-14 14:12:33 +0300

    Activate project

--- toolkit/core/project/views.py
+++ toolkit/core/project/views.py
@@ -1,8 +1,15 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.decorators import action
 
 from toolkit.core.project.models import Project
 from toolkit.core.project.serializers import ProjectSerializer
 
+from toolkit.core.user_profile.serializers import UserProfileSerializer
 
 # Create your views here.
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -11,3 +18,10 @@ class ProjectViewSet(viewsets.ModelViewSet):
     """
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+    # TODO custom permission for activating dataset thats listed to you?
+    @action(detail=True, methods=['put'])
+    def activate_project(self, request, pk=None):
+        obj = self.get_object()
+        request.user.profile.activate_project(obj)
+        return Response({'status': f'Project {pk} successfully activated.'}, status=status.HTTP_200_OK)
--- toolkit/core/urls.py
+++ toolkit/core/urls.py
@@ -6,7 +6,3 @@ from toolkit.core.project import views as project_views
 router = routers.DefaultRouter()
 router.register('user', profile_views.UserProfileViewSet)
 router.register('project', project_views.ProjectViewSet, base_name='project')
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
