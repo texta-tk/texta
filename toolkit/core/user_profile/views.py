9ca02bf6b850184eb172a02a729fe93210eba0fb
Your Name <you@example.com>
2019-05-16 15:27:50 +0300

    User profile permissions

--- toolkit/core/user_profile/views.py
+++ toolkit/core/user_profile/views.py
@@ -1,10 +1,34 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from rest_framework import viewsets
 
 from toolkit.core.user_profile.models import UserProfile
 from toolkit.core.user_profile.serializers import UserProfileSerializer
+from toolkit.core import permissions
 
 
 class UserProfileViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = UserProfile.objects.all().order_by('-user__date_joined')
     serializer_class = UserProfileSerializer
+
+    def get_queryset(self):
+        assert self.queryset is not None, (
+            "'%s' should either include a `queryset` attribute, "
+            "or override the `get_queryset()` method."
+            % self.__class__.__name__
+        )
+        current_user = self.get_current_user_id(self.request)
+        user_obj = self.get_current_user_obj(self.request)
+        queryset = self.queryset
+        if isinstance(queryset, QuerySet):
+            # re-evaluate queryset on each request.
+            queryset = queryset.all()
+        if not user_obj.is_superuser:
+            queryset = queryset[:].filter(user_id=current_user)
+        return queryset
+
+    def get_current_user_id(self, request):
+        return request.user.id
+
+    def get_current_user_obj(self, request):
+        return request.user
