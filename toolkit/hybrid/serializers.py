ec6482a7ede593a124145a3b25ba6d47d41a741f
Unknown <rsirel@ut.ee>
2019-05-28 12:37:45 +0300

    Auto stash before merge of "hybrid" and "origin/master"

--- toolkit/hybrid/serializers.py
+++ toolkit/hybrid/serializers.py
@@ -21,10 +21,16 @@ class HybridTaggerSerializer(serializers.HyperlinkedModelSerializer):
     minimum_sample_size = serializers.ChoiceField(choices=HYBRID_TAGGER_CHOICES['min_freq'])
     fact_name = serializers.ChoiceField(choices=get_fact_names())
     taggers = serializers.HyperlinkedRelatedField(read_only=True, many=True, view_name='tagger-detail')
-
     tagger = TaggerSerializer(write_only=True)
+
+    completed_taggers = serializers.SerializerMethodField()
 
     class Meta:
         model = HybridTagger
-        fields = ('id', 'project', 'author', 'description', 'fact_name', 'minimum_sample_size', 'taggers', 'tagger')
-        read_only_fields = ('author', 'project', 'taggers')
+        fields = ('id', 'project', 'author', 'description', 'fact_name', 'minimum_sample_size', 'taggers', 'completed_taggers', 'tagger')
+        read_only_fields = ('author', 'project', 'taggers', 'completed_taggers')
+    
+    def get_completed_taggers(self, obj):
+        total_taggers = HybridTagger.objects.get(id=obj.id).taggers
+        completed_taggers = total_taggers.filter(task__status='completed')
+        return '{0}/{1}'.format(len(completed_taggers), len(total_taggers.all()))
