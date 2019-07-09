82494e390feab1aaada68d03f89e83b2f0186896
rsirel <rsirel@texta.ee>
2019-07-09 15:41:50 +0300

    get fields to dict & get fields tests

---
+++ toolkit/core/project/tests/test_views.py
@@ -0,0 +1,26 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from toolkit.core.project.models import Project
+from toolkit.tools.utils_for_tests import create_test_user, print_output
+from toolkit import permissions as toolkit_permissions
+from toolkit.test_settings import TEST_INDEX
+
+
+class ProjectViewTests(APITestCase):
+
+    def setUp(self):
+        # Create a new User
+        self.user = create_test_user(name='user', password='pw')
+        self.project = Project.objects.create(title='testproj', owner=self.user, indices=[TEST_INDEX])
+        self.client = APIClient()
+        self.client.login(username='user', password='pw')
+
+    def test_get_fields(self):
+        url = f'/projects/{self.project.id}/get_fields/'
+        response = self.client.get(url)
+        print_output('get_fields:response.data', response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(isinstance(response.data, dict))
+        self.assertTrue(TEST_INDEX in response.data)
--- toolkit/core/project/views.py
+++ toolkit/core/project/views.py
@@ -37,4 +37,11 @@ class ProjectViewSet(viewsets.ModelViewSet):
         if not project_indices:
             return Response({'error': 'project has no indices'}, status=status.HTTP_400_BAD_REQUEST)
         fields = ElasticCore().get_fields(indices=project_indices)
-        return Response(fields, status=status.HTTP_200_OK)
+        field_map = {}
+        for field in fields:
+            if field['index'] not in field_map:
+                field_map[field['index']] = []
+                field_info = dict(field)
+                del field_info['index']
+            field_map[field['index']].append(field_info)
+        return Response(field_map, status=status.HTTP_200_OK)
