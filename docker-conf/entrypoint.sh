2c436c941baf4e9eaddf41dd3ac7771301f72742
rsirel <rsirel@texta.ee>
2019-07-24 11:51:37 +0300

    add docker compose & fixes

--- docker-conf/entrypoint.sh
+++ docker-conf/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-python migrate.py && python manage.py collectstatic
+python migrate.py && python manage.py collectstatic --noinput --clear
 chown www-data:www-data -R /var/texta-rest/static/ && chmod 777 -R /var/texta-rest/static/
-chown www-data:www-data -R /var/texta-rest/data/ && chmod 777 -R /var/texta-rest/data/
+#chown www-data:www-data -R /var/texta-rest/data/ && chmod 777 -R /var/texta-rest/data/
 
 # Get the maximum upload file size for Nginx, default to 0: unlimited
 USE_NGINX_MAX_UPLOAD=${NGINX_MAX_UPLOAD:-0}
 # Generate Nginx config for maximum upload file size
 echo "client_max_body_size $USE_NGINX_MAX_UPLOAD;" > /opt/conda/envs/texta-rest/etc/nginx/conf.d/upload.conf
-
-# Get the number of workers for Nginx, default to 1
-#USE_NGINX_WORKER_PROCESSES=${NGINX_WORKER_PROCESSES:-1}
-# Modify the number of worker processes in Nginx config
-#sed -i "/worker_processes\s/c\worker_processes ${USE_NGINX_WORKER_PROCESSES};"  /opt/conda/envs/texta-rest/etc/nginx/nginx.conf
 
 # Set the max number of connections per worker for Nginx, if requested
 # Cannot exceed worker_rlimit_nofile, see NGINX_WORKER_OPEN_FILES below
