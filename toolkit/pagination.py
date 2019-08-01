82020b14d93e525829bcf5680d053adc92fd41e1
Your Name <you@example.com>
2019-08-01 14:46:16 +0300

    Add comment

--- toolkit/pagination.py
+++ toolkit/pagination.py
@@ -2,6 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class PageNumberPaginationDataOnly(PageNumberPagination):
+    """ Default pagination """
 
     def get_paginated_response(self, data):
         return Response(data)
