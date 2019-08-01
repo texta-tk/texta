a1cd57155c5f82474f46eecde4d1ce407da97088
Unknown <rsirel@ut.ee>
2019-08-01 11:47:27 +0300

    phrases to list of strings

--- toolkit/core/lexicon/serializers.py
+++ toolkit/core/lexicon/serializers.py
@@ -3,6 +3,9 @@ from rest_framework import serializers
 from toolkit.core.lexicon.models import Lexicon
 
 class LexiconSerializer(serializers.ModelSerializer):
+
+    phrases = serializers.ListField(child=serializers.CharField(), help_text=f'Phrases as list of strings.')
+
     class Meta:
         model = Lexicon
         fields = ('id', 'author', 'description', 'phrases')
