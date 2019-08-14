0f316aeec44989daf03ec850475f7aa35568be1e
Your Name <you@example.com>
2019-08-14 14:53:41 +0300

    Project users can't perform unsafe methods on pinstance page

--- toolkit/core/permissions.py
+++ toolkit/core/permissions.py
@@ -4,6 +4,15 @@ from toolkit.core.project.models import Project
 class ProjectResourceAllowed(permissions.BasePermission):
     message = 'Insufficient permissions for this resource.'
 
+    """
+    All authenticated users can create new projects
+    All project users, and the project owner can perform SAFE and UNSAFE_METHODS on project resources.
+    Superuser has no restrictions.
+    If current_user is not a project user or owner, the project is filtered out in the queryset. So in most or all cases ProjectResourcepermissions
+    are not actually required. With future use cases, however, they may become useful.
+
+    """
+
     def has_permission(self, request, view):
         return self._permission_check(request, view)
 
--- toolkit/core/project/permissions.py
+++ toolkit/core/project/permissions.py
@@ -5,16 +5,18 @@ class ProjectAllowed(permissions.BasePermission):
     message = 'Insufficient permissions for this project.'
 
     def has_object_permission(self, request, view, obj):
+        return self._permission_check(request, view)
+
+    def _permission_check(self, request, view):
+        # always permit SAFE_METHODS and superuser
+        if request.user.is_superuser or request.method in permissions.SAFE_METHODS:
+            return True
         # retrieve project object
         try:
             project_object = Project.objects.get(id=view.kwargs['pk'])
         except:
             return False
-        # check if user is owner or listed in project users
-        if request.user in project_object.users.all() or request.user == project_object.owner:
+        if request.user in project_object.users.all() and request.method in permissions.SAFE_METHODS:
             return True
-        # check if user is superuser
-        if request.user.is_superuser:
-            return True
-        # nah, not gonna see anything!
-        return False
+        # if user is owner, allow UNSAFE_METHODS
+        return project_object.owner == request.user
