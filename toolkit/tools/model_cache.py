71d3ab972ee59f89a3a182fb51d5c8420ea9d20f
Unknown <rsirel@ut.ee>
2019-09-03 10:29:03 +0300

    model cache memory fix

--- toolkit/tools/model_cache.py
+++ toolkit/tools/model_cache.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from time import time
+from psutil import virtual_memory
 
 class ModelCache:
     """
@@ -12,17 +13,19 @@ class ModelCache:
 
 
     def get_model(self, model_id):
+        # check memory availability
+        self.check_memory()
         # load model if not in cache
         try:
             if model_id not in self.models:
                 model = self.object_class(model_id)
                 model.load()
-                self.models[model_id] = {'model': model, 'last_access': time()}
+                self.models[model_id] = {"model": model, "last_access": time()}
             # update last access timestamp & remove old models
-            self.models[model_id]['last_access'] = time()
+            self.models[model_id]["last_access"] = time()
             self.clean_cache()
             # return model
-            return self.models[model_id]['model']
+            return self.models[model_id]["model"]
         except Exception as e:
             print("Error loading modal to cache:", e)
             return None
@@ -30,4 +33,15 @@ class ModelCache:
 
     def clean_cache(self):
         # removes models not accessed in last 60 minutes (default)
-        self.models = {k:v for k,v in self.models.items() if v['last_access'] >= time()-self.cache_duration}
+        self.models = {k:v for k,v in self.models.items() if v["last_access"] >= time()-self.cache_duration}
+
+
+    def check_memory(self):
+        """
+        Checks memory availability and flushes cache if memory full.
+        """
+        memory = virtual_memory()
+        # if less than 5% free, flush the models to make room for more
+        if memory.available/memory.total < 0.05:
+            print("Warning: Memory full, flushed models to make room.")
+            self.models = {}
