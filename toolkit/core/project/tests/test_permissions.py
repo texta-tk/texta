ded1bad3d00ef2b7e242c7eed3785f5f0a91217c
Your Name <you@example.com>
2019-09-05 14:10:28 +0300

    Permissions tests

--- toolkit/core/project/tests/test_permissions.py
+++ toolkit/core/project/tests/test_permissions.py
@@ -9,34 +9,76 @@ from toolkit import permissions as toolkit_permissions
 
 class ProjectPermissionsTests(APITestCase):
 
+
     def setUp(self):
-        # Create a new User
-        self.user_1 = create_test_user(name='user_1', password='pw')
-        self.user_2 = create_test_user(name='user_2', password='pw')
-        self.user_3 = create_test_user(name='user_3', password='pw')
-        self.project = Project.objects.create(title='testproj', owner=self.user_1)
-        self.project.users.add(self.user_2)
-        self.project.users.add(self.user_1)
+        self.admin = create_test_user(name='admin', password='1234')
+        self.admin.is_superuser = True
+        self.admin.save()
+        self.owner = create_test_user(name='owner', password='pw')
+        self.project_user = create_test_user(name='project_user', password='pw')
+        self.user = create_test_user(name='user', password='pw')
+        self.project = Project.objects.create(title='testproj', owner=self.owner)
+        self.project.users.add(self.owner)
+        self.project.users.add(self.project_user)
         self.client = APIClient()
+        self.project_list_url = f'/projects/'
+        self.project_instance_url = f'/projects/{self.project.id}/'
 
     def test_all(self):
         for resource in ('lexicons', 'taggers', 'embeddings', 'embedding_clusters', 'tagger_groups'):
-            self.run_project_with_user_listed(resource)
-            self.run_project_with_user_not_listed(resource)
+            self.project_resource_url = f'/projects/{self.project.id}/{resource}/'
+            self.run_with_users(self.access_project_resources, resource)
+        self.run_with_users(self.access_project_instance_methods)
+        self.run_with_users(self.update_project_fields)
 
-    def run_project_with_user_listed(self, resource):
-        url = f'/projects/{self.project.id}/{resource}/'
-        '''Test if listed users allowed to see content'''
-        self.client.login(username='user_1', password='pw')
+    def run_with_users(self, func, resource=None):
+        if resource is None:
+            func(self.admin, '1234')
+            func(self.project.owner, self.project.owner.password)
+            func(self.project_user, 'pw', UNSAFE_FORBIDDEN=True)
+            func(self.user, 'pw', SAFE_FORBIDDEN=True, UNSAFE_FORBIDDEN=True)
+        else:
+            func(self.admin, '1234')
+            func(self.project.owner, self.project.owner.password)
+            func(self.project_user, 'pw')
+            func(self.user, 'pw', fail=True)
+
+    def access_project_resources(self, username, password, fail=False):
+        url = self.project_resource_url
+        self.client.login(username=username, password=password)
         response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.client.login(username='user_2', password='pw')
-        response = self.client.get(url)
+        if fail is True:
+            return self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def run_project_with_user_not_listed(self, resource):
-        '''Test if unlisted users rejected'''
-        url = f'/projects/{self.project.id}/{resource}/'
-        self.client.login(username='user_3', password='pw')
+    def access_project_instance_methods(self, username, password, SAFE_FORBIDDEN=False, UNSAFE_FORBIDDEN=False):
+        '''F, F for owner,admin, F, T for p_user, F, F for non-member.'''
+        url = self.project_instance_url
+        self.client.login(username=username, password=password)
+        get_response = self.client.get(url)
+        responses = {'GET': get_response, 'PUT': self.client.put(url, get_response.data, format='json')}
+        for response in list(responses.values()):
+            if SAFE_FORBIDDEN is False and UNSAFE_FORBIDDEN is False:
+                self.assertEqual(response.status_code, status.HTTP_200_OK)
+            if SAFE_FORBIDDEN is True and UNSAFE_FORBIDDEN is True:
+                self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        if SAFE_FORBIDDEN is False and UNSAFE_FORBIDDEN is True:
+            self.assertEqual(responses['GET'].status_code, status.HTTP_200_OK)
+            self.assertEqual(responses['PUT'].status_code, status.HTTP_403_FORBIDDEN)
+
+    def update_project_fields(self, username, password, SAFE_FORBIDDEN=False, UNSAFE_FORBIDDEN=False):
+        url = self.project_instance_url
+        self.client.login(username=username, password=password)
         response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        response.data["indices"] =  {"texta_test_index"}
+        response.data["title"] = "put_title"
+        put_response = self.client.put(url, response.data, format='json')
+        if SAFE_FORBIDDEN is False and UNSAFE_FORBIDDEN is False:
+            self.assertEqual(put_response.status_code, status.HTTP_200_OK)
+        if SAFE_FORBIDDEN is True and UNSAFE_FORBIDDEN is True:
+                self.assertEqual(put_response.status_code, status.HTTP_404_NOT_FOUND)
+        if SAFE_FORBIDDEN is False and UNSAFE_FORBIDDEN is True:
+            self.assertEqual(put_response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+    # TODO: implement owner, user put testing separately; affects existing tests.
