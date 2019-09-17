d4409e1492e5d65201d985b3e3d44ac4233d8c80
unknown <ranetpuusepp@gmail.com>
2019-09-17 12:57:18 +0300

    Fixed merge conflict bug

--- toolkit/settings.py
+++ toolkit/settings.py
@@ -11,11 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-<<<<<<< HEAD
 import warnings
-=======
 from .logging_settings import setup_logging
->>>>>>> logging
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
