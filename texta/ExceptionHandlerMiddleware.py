1a5007e3c8e66c8cce063cf0c246528c36cfe147
Gert Paimla <gertpaimla@gmail.com>
2019-09-23 16:31:12 +0300

    add OperationalError clause to exceptionhandler

--- texta/ExceptionHandlerMiddleware.py
+++ texta/ExceptionHandlerMiddleware.py
@@ -6,6 +6,7 @@ from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
 
 from texta.settings import ERROR_LOGGER, STATIC_URL
+from django.db.utils import OperationalError
 
 
 class ExceptionHandlerMiddleware(MiddlewareMixin):
@@ -13,6 +14,12 @@ class ExceptionHandlerMiddleware(MiddlewareMixin):
         if isinstance(exception, requests.exceptions.ConnectionError):
             logging.getLogger(ERROR_LOGGER).exception(exception)
             messages.error(request, "Could not connect to resource: {}. Please check if all the resources (Elasticsearch) are available!".format(exception.request.url))
+            template_data = {'STATIC_URL': STATIC_URL, 'allowed_datasets': None, 'language_models': None}
+            return redirect("/", context=template_data)
+
+        if isinstance(exception, OperationalError):
+            logging.getLogger(ERROR_LOGGER).exception(exception)
+            messages.error(request, "Error, please refresh the page!".format(exception))
             template_data = {'STATIC_URL': STATIC_URL, 'allowed_datasets': None, 'language_models': None}
             return redirect("/", context=template_data)
 
