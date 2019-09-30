26f22c52c15c80c685bdfa1a0a9ae83b0e01ed63
rsirel <rsirel@texta.ee>
2019-09-30 19:53:35 +0300

    final touches

--- docker/build_and_push.py
+++ docker/build_and_push.py
@@ -13,15 +13,14 @@ def main():
             print("Building...")
             built = subprocess.Popen(build_command, shell=True, stdout=subprocess.PIPE)
             built_id = built.communicate()[0].strip().split('\n')[-2].split()[-1]
-            print(built_id)
             print("Tagging latest...")
             tag_command = "docker tag {} docker.texta.ee/texta/texta-rest:latest".format(built_id)
-            #tagged = subprocess.Popen(tag_command, shell=True)
-            #for tag in [version, "latest"]:
-            #    print("Pushing {}...".format(tag))
-            #    push_command = "docker push docker.texta.ee/texta/texta-rest:{}".format(tag)
-            #    pushed = subprocess.Popen(push_command, shell=True)
-            #    print("Pushed {}.".format(tag))
+            tagged = subprocess.Popen(tag_command, shell=True)
+            for tag in [version, "latest"]:
+                print("Pushing {}...".format(tag))
+                push_command = "docker push docker.texta.ee/texta/texta-rest:{}".format(tag)
+                pushed = subprocess.Popen(push_command, shell=True)
+                print("Pushed {}.".format(tag))
     except Exception as e:
         print("Build failed:", e)
         return
