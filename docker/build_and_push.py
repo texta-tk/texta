63b3bcda999638670aad4329b194d517944fca2e
rsirel <rsirel@texta.ee>
2019-09-30 19:51:47 +0300

    fix script

--- docker/build_and_push.py
+++ docker/build_and_push.py
@@ -8,19 +8,20 @@ def main():
     try:
         with open("VERSION") as fh:
             version = fh.read()
-            build_command = "docker build -t docker.texta.ee/texta/texta-rest:{}".format(version)
+            build_command = "docker build -t docker.texta.ee/texta/texta-rest:{} -f ./docker/Dockerfile .".format(version)
             print("Building, tagging and pushing Docker image for version {}.".format(version))
             print("Building...")
             built = subprocess.Popen(build_command, shell=True, stdout=subprocess.PIPE)
             built_id = built.communicate()[0].strip().split('\n')[-2].split()[-1]
+            print(built_id)
             print("Tagging latest...")
             tag_command = "docker tag {} docker.texta.ee/texta/texta-rest:latest".format(built_id)
-            tagged = subprocess.Popen(tag_command, shell=True)
-            for tag in [version, "latest"]:
-                print("Pushing {}...".format(tag))
-                push_command = "docker push docker.texta.ee/texta/texta-rest:{}".format(tag)
-                pushed = subprocess.Popen(push_command, shell=True)
-                print("Pushed {}.".format(tag))
+            #tagged = subprocess.Popen(tag_command, shell=True)
+            #for tag in [version, "latest"]:
+            #    print("Pushing {}...".format(tag))
+            #    push_command = "docker push docker.texta.ee/texta/texta-rest:{}".format(tag)
+            #    pushed = subprocess.Popen(push_command, shell=True)
+            #    print("Pushed {}.".format(tag))
     except Exception as e:
         print("Build failed:", e)
         return
