343255003eafc659f5355346230fd6d0574e19f2
rsirel <rsirel@texta.ee>
2019-10-01 08:27:58 +0300

    automate gpu build as well

--- docker/build_and_push.py
+++ docker/build_and_push.py
@@ -2,25 +2,36 @@ import subprocess
 import sys
 
 
+def build(project, version, tag_suffix=""):
+    # build image
+    build_command = "docker build -t docker.texta.ee/texta/{0}:{1}{2} -f ./docker/Dockerfile .".format(project, version, tag_suffix)
+    print("Building, tagging and pushing Docker image for version {}.".format(version))
+    print("Building...")
+    built = subprocess.Popen(build_command, shell=True, stdout=subprocess.PIPE)
+    built_id = built.communicate()[0].strip().split('\n')[-2].split()[-1]
+    # tag latest
+    print("Tagging latest...")
+    tag_command = "docker tag {0} docker.texta.ee/texta/{1}:latest{2}".format(built_id, project, tag_suffix)
+    tagged = subprocess.Popen(tag_command, shell=True)
+    # push
+    for tag in [version, "latest"]:
+        print("Pushing {}...".format(tag))
+        push_command = "docker push docker.texta.ee/texta/{0}:{1}{2}".format(project, tag, tag_suffix)
+        pushed = subprocess.Popen(push_command, shell=True)
+        print("Pushed {}.".format(tag))
+
 
 def main():
-    print("Using Python version: {}".format(sys.version))
     try:
+        # declare project name
+        project = "texta-rest"
+        # load version from file system
         with open("VERSION") as fh:
             version = fh.read()
-            build_command = "docker build -t docker.texta.ee/texta/texta-rest:{} -f ./docker/Dockerfile .".format(version)
-            print("Building, tagging and pushing Docker image for version {}.".format(version))
-            print("Building...")
-            built = subprocess.Popen(build_command, shell=True, stdout=subprocess.PIPE)
-            built_id = built.communicate()[0].strip().split('\n')[-2].split()[-1]
-            print("Tagging latest...")
-            tag_command = "docker tag {} docker.texta.ee/texta/texta-rest:latest".format(built_id)
-            tagged = subprocess.Popen(tag_command, shell=True)
-            for tag in [version, "latest"]:
-                print("Pushing {}...".format(tag))
-                push_command = "docker push docker.texta.ee/texta/texta-rest:{}".format(tag)
-                pushed = subprocess.Popen(push_command, shell=True)
-                print("Pushed {}.".format(tag))
+        # build CPU version
+        build(project, version)
+        # build GPU version
+        build(project, version, tag_suffix="-gpu")
     except Exception as e:
         print("Build failed:", e)
         return
