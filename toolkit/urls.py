3327987eeeb477297141045590c5c2d3c9601c14
rsirel <rsirel@texta.ee>
2019-10-03 13:58:19 +0300

    test fixes + some refacto

--- toolkit/urls.py
+++ toolkit/urls.py
@@ -14,10 +14,7 @@ from toolkit.core.project.views import ProjectViewSet
 from toolkit.core.user_profile.views import UserViewSet
 from toolkit.embedding.views import EmbeddingViewSet
 from toolkit.settings import MEDIA_DIR, MEDIA_URL
-
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from toolkit.tools.swagger import schema_view
 
 
 @login_required
@@ -36,28 +33,21 @@ project_router.registry.extend(core_router.registry)
 project_router.registry.extend(neurotagger_router.registry)
 
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="TEXTA Toolkit REST API",
-      default_version='v1',
-      description="TEXTA Toolkit REST API",
-      contact=openapi.Contact(email="info@texta.ee"),
-      license=openapi.License(name="GPLv3"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
-
-
 urlpatterns = [
+    # documentation
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # protected media
     url(r'^%s(?P<path>.*)$' % MEDIA_URL, protected_serve, {'document_root': MEDIA_DIR}),
+    # static
     url(r'static/(?P<path>.*)$',serve,{'document_root': 'static'}),
+    # health
     url('health', HealthView.as_view()),
+    # auth
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    # routers
     url(r'^', include(router.urls)),
     url(r'^', include(project_router.urls))
 ]
