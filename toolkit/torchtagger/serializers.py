217c8f3cae0bb9b2e0378cfc35a2cf831d047126
rsirel <rsirel@texta.ee>
2019-11-19 19:18:48 +0200

    update torch serializer

--- toolkit/torchtagger/serializers.py
+++ toolkit/torchtagger/serializers.py
@@ -9,14 +9,14 @@ from toolkit.core.task.serializers import TaskSerializer
 from toolkit.serializer_constants import ProjectResourceUrlSerializer, FieldParseSerializer
 
 
-class TorchTaggerSerializer(serializers.ModelSerializer, ProjectResourceUrlSerializer):
+class TorchTaggerSerializer(FieldParseSerializer, serializers.ModelSerializer, ProjectResourceUrlSerializer):
     author_username = serializers.CharField(source='author.username', read_only=True)
     fields = serializers.ListField(child=serializers.CharField(), help_text=f'Fields used to build the model.')
     query = serializers.JSONField(help_text='Query in JSON format', required=False)
     fact_name = serializers.CharField(default=None, required=False, help_text=f'Fact name used to filter tags (fact values). Default: None')
     model_architecture = serializers.ChoiceField(choices=choices.MODEL_CHOICES)
-    maximum_sample_size = serializers.IntegerField(default=10000, required=False)
-    minimum_sample_size = serializers.IntegerField(default=50, required=False)
+    maximum_sample_size = serializers.IntegerField(default=choices.DEFAULT_MAX_SAMPLE_SIZE, required=False)
+    minimum_sample_size = serializers.IntegerField(default=choices.DEFAULT_MIN_SAMPLE_SIZE, required=False)
     num_epochs = serializers.IntegerField(default=choices.DEFAULT_NUM_EPOCHS, required=False)
 
     task = TaskSerializer(read_only=True)
@@ -30,4 +30,4 @@ class TorchTaggerSerializer(serializers.ModelSerializer, ProjectResourceUrlSeria
             'model_architecture', 'maximum_sample_size', 'minimum_sample_size', 'num_epochs', 'location', 'plot', 'task', 'fact_name', 'epoch_reports',
         )
         read_only_fields = ('project', 'fields', 'f1_score', 'precision', 'recall', 'accuracy', 'location', 'plot', 'task' ,'fact_name', 'epoch_reports')
-        fields_to_parse = ('fields', 'location')
+        fields_to_parse = ('fields', 'location', 'epoch_reports')
