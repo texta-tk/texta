30da507e6ea6525c0e719010c082dfe663559922
Erik <erik.jurmann@gmail.com>
2019-11-27 15:05:19 +0200

    Add superuser boolean to user profile

--- toolkit/core/user_profile/serializers.py
+++ toolkit/core/user_profile/serializers.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+import json
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    is_superuser = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('url', 'id', 'username', 'email', 'date_joined')
+        fields = ('url', 'id', 'username', 'email', 'date_joined', 'is_superuser')
         read_only_fields = ('username', 'email', 'date_joined')
+
+    def get_is_superuser(self, obj):
+        return obj.is_superuser
