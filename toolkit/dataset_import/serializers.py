9d2b3a3ef5e15358ddc7add0dc51d5cf311cfa6e
Unknown <rsirel@ut.ee>
2019-12-03 11:39:51 +0200

    add migrations. add separator

--- toolkit/dataset_import/serializers.py
+++ toolkit/dataset_import/serializers.py
@@ -9,12 +9,13 @@ from toolkit.serializer_constants import ProjectResourceUrlSerializer, FieldPars
 class DatasetImportSerializer(FieldParseSerializer, serializers.HyperlinkedModelSerializer, ProjectResourceUrlSerializer):
     author_username = serializers.CharField(source='author.username', read_only=True)
     file = serializers.FileField(help_text='File to upload.', write_only=True)
+    separator = serializers.CharField(help_text='Separator (CSV only).', required=False)
     index = serializers.CharField(help_text='Index to upload dataset into.')
     task = TaskSerializer(read_only=True)
     url = serializers.SerializerMethodField()
 
     class Meta:
         model = DatasetImport
-        fields = ('id', 'url', 'author_username', 'description', 'index', 'num_documents', 'file', 'task')
+        fields = ('id', 'url', 'author_username', 'description', 'index', 'separator', 'num_documents', 'file', 'task')
         fields_to_parse = ()
         read_only_fields = ('id', 'author_username', 'num_documents', 'task')
