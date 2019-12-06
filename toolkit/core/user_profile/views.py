6d2ae457d6ec783d9414acb3cb52f2140ed2baf3
Erik <erik.jurmann@gmail.com>
2019-12-06 12:46:22 +0200

    Assign admin toggle

--- toolkit/core/user_profile/views.py
+++ toolkit/core/user_profile/views.py
@@ -5,9 +5,11 @@ from django.contrib.auth.models import User
 from toolkit.core.project.models import Project
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from toolkit.core.user_profile.models import UserProfile
 from toolkit.core.user_profile.serializers import UserSerializer
+from toolkit.permissions.project_permissions import IsSuperUser
 
 
 
@@ -29,9 +31,15 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
             queryset = (queryset.filter(id=self.request.user.id) | queryset.filter(project_users__in=projects)).distinct()
         return queryset
 
-    def handle_exception(self, exc):
-        if isinstance(exc, Http404):
-            return Response({'detail': 'Insufficient permissions for this resource.'},
-                            status=status.HTTP_404_NOT_FOUND)
+    @action(detail=True, methods=['get', 'post'], permission_classes=[IsSuperUser])
+    def assign_superuser(self, request, pk=None):
+        user = User.objects.get(id=self.kwargs['pk'])
+        user.is_superuser ^= True   # toggle
+        user.save()
+        if user.is_superuser:
+            return Response({"detail": "Superuser status assigned"}, status=status.HTTP_200_OK)
+        return Response({"detail": "Superuser status removed"}, status=status.HTTP_200_OK)
 
-        return super(ProjectDetails, self).handle_exception(exc)
+
+
+
--- toolkit/permissions/project_permissions.py
+++ toolkit/permissions/project_permissions.py
@@ -59,7 +59,14 @@ class ProjectAllowed(permissions.BasePermission):
 
 class IsSuperUser(permissions.BasePermission):
 
+
     def has_permission(self, request, view):
+        return self._permission_check(request, view)
+
+    def has_object_permission(self, request, view, obj):
+        return self._permission_check(request, view)
+
+    def _permission_check(self, request, view):
         return request.user and request.user.is_superuser
 
 
