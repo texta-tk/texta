ab61813232ce4165b7a924655aab9a66f3f16aa4
Marko Kollo <mrxkollo@users.noreply.github.com>
2019-12-20 11:39:23 +0200

    Changing CSRF values to be compliant with Angulars default values.

--- toolkit/settings.py
+++ toolkit/settings.py
@@ -30,6 +30,10 @@ DEBUG = eval(os.getenv('TEXTA_DEBUG', "True"))
 
 ALLOWED_HOSTS = ['*']
 
+
+CSRF_HEADER_NAME = "X-XSRF-TOKEN"
+CSRF_COOKIE_NAME = "XSRF-TOKEN"
+
 # Application definition
 
 INSTALLED_APPS = [
