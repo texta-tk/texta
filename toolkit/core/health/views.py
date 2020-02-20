0240186a63edf4e75f982fa36389f702eb790ab1
Marko Kollo <mkollo@tlu.ee>
2020-02-20 16:29:17 +0200

    Added redis to health and changed progress db handling.

--- toolkit/core/health/views.py
+++ toolkit/core/health/views.py
@@ -1,17 +1,41 @@
 import shutil
+from urllib.parse import urlparse
 
 import psutil
+import redis
 import torch
-from rest_framework import views, status
+from rest_framework import status, views
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from toolkit.core.health.utils import get_active_tasks, get_elastic_status, get_mlp_status, get_version
+from toolkit.settings import BROKER_URL
 
 
 @permission_classes((AllowAny,))
 class HealthView(views.APIView):
+
+    def redis_status(self):
+        try:
+            parser = urlparse(BROKER_URL)
+            r = redis.Redis(host=parser.hostname, port=parser.port)
+            info = r.info()
+            redis_status = {
+                "ali    ve": True,
+                "version": info["redis_version"],
+                "expired_keys": info["expired_keys"],
+                "used_memory": info["used_memory_human"],
+                "total_memory": info["total_system_memory_human"]
+            }
+            return redis_status
+
+        except redis.exceptions.ConnectionError:
+            return {
+                "alive": False
+            }
+
+
     def get(self, request):
         """Returns health statistics about host machine and running services."""
         toolkit_status = {}
@@ -38,6 +62,8 @@ class HealthView(views.APIView):
         }
 
         toolkit_status['cpu'] = {'percent': psutil.cpu_percent()}
+
+        toolkit_status["redis"] = self.redis_status()
 
         gpu_count = torch.cuda.device_count()
         gpu_devices = [torch.cuda.get_device_name(i) for i in range(0, gpu_count)]
--- toolkit/tools/show_progress.py
+++ toolkit/tools/show_progress.py
@@ -1,23 +1,29 @@
-from toolkit.core.task.models import Task
 import json
+import logging
+
+from toolkit.core.task.models import Task
+from toolkit.settings import ERROR_LOGGER
 
 
 class ShowProgress(object):
     """ Show model training progress
     """
 
+
     def __init__(self, task, multiplier=None):
         self.n_total = None
         self.n_count = 0
-        self.task = task
+        self.task_id = task.id
         self.multiplier = multiplier
         self.step = ''
+
 
     def set_total(self, total):
         self.n_count = 0
         self.n_total = total
         if self.multiplier:
             self.n_total = self.multiplier * total
+
 
     def update_step(self, step):
         self.step = step
@@ -31,16 +37,23 @@ class ShowProgress(object):
 
         self.update_view(percentage)
 
+
     def update_view(self, percentage):
-        if self.task.status != self.task.STATUS_RUNNING:
-            self.task.update_status(self.task.STATUS_RUNNING)
-        self.task.update_progress(percentage, self.step)
+        task = Task.objects.get(pk=self.task_id)
+        if task.status != task.STATUS_RUNNING:
+            task.update_status(task.STATUS_RUNNING)
+        task.update_progress(percentage, self.step)
+
 
     def update_errors(self, error):
+        task = Task.objects.get(pk=self.task_id)
+
         try:
-            errors_json = json.loads(self.task.errors)
-        except:
+            errors_json = json.loads(task.errors)
+        except Exception as e:
+            logging.getLogger(ERROR_LOGGER).exception(e)
             errors_json = []
+
         errors_json.append('Error at {0}: {1}'.format(self.step, error))
-        self.task.errors = json.dumps(errors_json)
-        self.task.save()
+        task.errors = json.dumps(errors_json)
+        task.save()
