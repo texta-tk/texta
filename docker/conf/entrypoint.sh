c4acec6c30779eb5dc1586970c943a71982935b8
Raul Sirel <rsirel@texta.ee>
2020-05-13 20:59:20 +0300

    Update entrypoint.sh

--- docker/conf/entrypoint.sh
+++ docker/conf/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export ENV_TEXTA_SHORT_TASK_WORKERS="${TEXTA_SHORT_TASK_WORKERS:-3}"
-export ENV_TEXTA_LONG_TASK_WORKERS="${TEXTA_LONG_TASK_WORKERS:-5}"
+export TEXTA_SHORT_TASK_WORKERS="${TEXTA_SHORT_TASK_WORKERS:-3}"
+export TEXTA_LONG_TASK_WORKERS="${TEXTA_LONG_TASK_WORKERS:-5}"
 
 source activate texta-rest && python migrate.py
 source activate texta-rest && python manage.py collectstatic --noinput --clear
