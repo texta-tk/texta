3623ea3b3b0b0b774b155ac57e148d847f4e57fc
Marko Kollo <mrxkollo@users.noreply.github.com>
2020-05-13 18:04:36 +0300

    Test version of using multiple celery workers in supervisord.

--- docker/conf/entrypoint.sh
+++ docker/conf/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+export TEXTA_SHORT_TASK_WORKERS="${TEXTA_SHORT_TASK_WORKERS:-3}"
+export TEXTA_LONG_TASK_WORKERS="${TEXTA_LONG_TASK_WORKERS:-5}"
+
 source activate texta-rest && python migrate.py
 source activate texta-rest && python manage.py collectstatic --noinput --clear
 chown www-data:www-data -R /var/texta-rest/static/ && chmod 777 -R /var/texta-rest/static/
 chown www-data:www-data -R /var/texta-rest/data/ && chmod 777 -R /var/texta-rest/data/
 
 set | egrep "^(DJANGO|TEXTA|PYTHON|LC_|LANG)" | sed -e 's/^/export /' > .env
+
 
 exec "$@"
