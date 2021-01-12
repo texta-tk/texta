0e4717e27310ec1145df02f8b13c9586a144bc2a
Linda Freienthal <linda.freienthal96@gmail.com>
2021-01-12 16:15:34 +0200

    changed elastic.searcher and tests

--- toolkit/embedding/tasks.py
+++ toolkit/embedding/tasks.py
@@ -33,8 +33,11 @@ def train_embedding(embedding_id):
                                     indices=indices,
                                     field_data=field_data,
                                     callback_progress=show_progress,
-                                    scroll_limit=max_documents)
+                                    scroll_limit=max_documents,
+                                    text_processor=TextProcessor(sentences=True, remove_stop_words=True, words_as_list=True),
+                                    output=ElasticSearcher.OUT_TEXT)
         # create embedding object & train
+        print(sentences)
         embedding = W2VEmbedding()
         embedding.train(sentences, use_phraser=use_phraser)
 
