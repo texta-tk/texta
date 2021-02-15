a6f347254c2dfd4f8f817e54e1681abd8d96ea01
hele <kuulmets@gmail.com>
2021-02-15 13:09:11 +0200

    query validation #352

--- toolkit/elastic/serializers.py
+++ toolkit/elastic/serializers.py
@@ -177,7 +177,7 @@ class IndexSplitterSerializer(FieldParseSerializer, serializers.HyperlinkedModel
     scroll_size = serializers.IntegerField(min_value=0, max_value=10000, required=False)
     description = serializers.CharField(help_text='Description of the task.', required=True, allow_blank=False)
     indices = IndexSerializer(many=True, write_only=True, default=[], help_text=f'Indices that are used to create train and test indices.')
-    query = serializers.JSONField(help_text='Query used to filter the indices. Defaults to an empty query.', required=False)
+    query = serializers.JSONField(help_text='Query used to filter the indices. Defaults to an empty query.', required=False, default=EMPTY_QUERY)
     train_index = serializers.CharField(help_text='Name of the train index.', allow_blank=False, required=True,
     validators=[
             check_for_wildcards,
@@ -249,6 +249,13 @@ class IndexSplitterSerializer(FieldParseSerializer, serializers.HyperlinkedModel
             if field not in field_data:
                 raise serializers.ValidationError(f'The fields you are attempting to add to new indices are not in current project fields: {project_fields}')
         return value
+
+
+    def validate_query(self, value):
+        if "query" not in value:
+            raise serializers.ValidationError("Incorrect elastic query. Must contain field 'query'.")
+        return value
+
 
     def validate(self, data):
         fact = data.get("fact")
