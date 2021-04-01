79c1dc016bdf7de4e9e8988a5ce06665c57efb3e
Raul Sirel <rsirel@texta.ee>
2021-04-01 15:33:43 +0300

    hotfix for stemming long texts with elastic

--- toolkit/tools/lemmatizer.py
+++ toolkit/tools/lemmatizer.py
@@ -1,4 +1,5 @@
 from celery.result import allow_join_result
+from texta_tools.text_splitter import TextSplitter
 
 from toolkit.mlp.tasks import apply_mlp_on_list
 from toolkit.settings import CELERY_MLP_TASK_QUEUE
@@ -24,21 +25,30 @@ class ElasticLemmatizer:
     def __init__(self, language="english"):
         self.core = ElasticCore()
         self.indices_client = IndicesClient(self.core.es)
+        self.splitter = TextSplitter()
         self.snowball_filter = {"type": "snowball", "language": language}
 
     def lemmatize(self, text):
-        body = {
-            "tokenizer": "standard",
-            "text": text,
-            "filter": [self.snowball_filter]
-            
-        }
-        try:
-            analysis = self.indices_client.analyze(body=body)
-        except:
-            raise ElasticSnowballException("Snowball failed. Check Connection & payload!")
-
-        tokens = [token["token"] for token in analysis["tokens"]]
-        token_string = " ".join(tokens)
-
-        return token_string
+        analyzed_chunks = []
+        # split input if token count greater than 5K
+        # elastic will complain if token count exceeds 10K
+        docs = self.splitter.split(text, max_limit=5000)
+        # extract text chunks from docs
+        text_chunks = [doc["text"] for doc in docs]
+        # analyze text chunks
+        for text in text_chunks:
+            body = {
+                "tokenizer": "standard",
+                "text": text,
+                "filter": [self.snowball_filter] 
+            }
+            try:
+                analysis = self.indices_client.analyze(body=body)
+            except:
+                raise ElasticSnowballException("Snowball failed. Check Connection & payload!")
+            # tokens back to text chunk
+            tokens = [token["token"] for token in analysis["tokens"]]
+            token_string = " ".join(tokens)
+            analyzed_chunks.append(token_string)
+        # return chunks as string
+        return " ".join(analyzed_chunks)
