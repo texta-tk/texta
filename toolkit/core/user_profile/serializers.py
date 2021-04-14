f5d92dffdbd67588b91df08f5a39eb8b11e55222
Raul Sirel <rsirel@texta.ee>
2021-04-14 11:37:43 +0300

    add profile to user serializer

--- toolkit/core/user_profile/serializers.py
+++ toolkit/core/user_profile/serializers.py
@@ -1,10 +1,21 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import UserProfile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserProfile
+        fields = ('application',)
+        read_only_fields = ('application',)
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    profile = ProfileSerializer(read_only=True)
 
     class Meta:
         model = User
-        fields = ('url', 'id', 'username', 'email', 'date_joined', 'is_superuser', 'last_login')
-        read_only_fields = ('username', 'email', 'date_joined', 'last_login')
+        fields = ('url', 'id', 'username', 'email', 'date_joined', 'is_superuser', 'last_login', 'profile')
+        read_only_fields = ('username', 'email', 'date_joined', 'last_login', 'profile')
+
