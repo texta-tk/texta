809c55696121923c5ae24fb15f2e4e258811e64f
Wael <wael@texta.ee>
2021-06-10 12:31:03 +0300

    UAA user profile scope

--- toolkit/core/user_profile/models.py
+++ toolkit/core/user_profile/models.py
@@ -6,6 +6,7 @@ from toolkit.constants import MAX_DESC_LEN
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    scope = models.CharField(max_length=MAX_DESC_LEN, default="None")
     # define the application user is used for (e.g. "toolkit" and "law")
     application = models.CharField(max_length=MAX_DESC_LEN, default="toolkit")
 
