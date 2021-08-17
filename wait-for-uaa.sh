2d541c087385a1d3b7c30d70438ed1b119871079
Gert Paimla <gertpaimla@gmail.com>
2021-08-17 14:49:24 +0000

    Update wait-for-uaa.sh

--- wait-for-uaa.sh
+++ wait-for-uaa.sh
@@ -3,7 +3,7 @@
 export TEXTA_UAA_URL="${TEXTA_UAA_URL:-http://localhost:8080}"
 
 # wait for server to start
-until $(curl --output /dev/null --silent --head --fail "$TEXTA_UAA_URL/uaa"); do
+until $(curl --output /dev/null --silent --head --fail "$TEXTA_UAA_URL"/uaa); do
     echo "Waiting for UAA server to run."
     sleep 5
 done
