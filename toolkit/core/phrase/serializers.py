0766aba18a05d4d386da375cb8fb5f8c6fa71c66
Raul Sirel <rsirel@texta.ee>
2021-10-06 16:20:32 +0300

    delete phrase + migrations

--- toolkit/core/phrase/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from toolkit.core.phrase.models import Phrase
-
-class PhraseSerializer(serializers.HyperlinkedModelSerializer):
-    
-    class Meta:
-        model = Phrase
-        fields = ('url', 'id', 'project', 'author', 'phrase')
