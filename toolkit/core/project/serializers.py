797bc78998cbcef0e7007b2abf1f282856a8b570
wael <wael@texta.ee>
2021-10-20 16:26:02 +0300

    add help_text for fields

--- toolkit/core/project/serializers.py
+++ toolkit/core/project/serializers.py
@@ -22,7 +22,7 @@ from toolkit.serializer_constants import FieldParseSerializer, IndicesSerializer
 class ExportSearcherResultsSerializer(serializers.Serializer):
     indices = serializers.ListField(child=serializers.CharField(), default=[])
     query = serializers.JSONField(help_text="Which query results to fetch.", default=EMPTY_QUERY)
-    fields = serializers.ListField(child=serializers.CharField(), default=[])
+    fields = serializers.ListField(help_text="Which fields to output.", child=serializers.CharField(), default=[])
 
 
 class ProjectSearchByQuerySerializer(serializers.Serializer):
