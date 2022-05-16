265f536aa2f7118d9bc04336fe3cfcae3546cdff
Wael <wael@texta.ee>
2022-05-16 11:56:25 +0300

    feat: create parent annotator object before task

--- toolkit/annotator/models.py
+++ toolkit/annotator/models.py
@@ -122,6 +122,10 @@ class Annotator(TaskModel):
         self.task = new_task
         self.save()
 
+        annotator_obj = Annotator.objects.get(pk=self.pk)
+        annotator_group, is_created = AnnotatorGroup.objects.get_or_create(project=annotator_obj.project,
+                                                                           parent=annotator_obj)
+
         from toolkit.annotator.tasks import annotator_task
         annotator_task.apply_async(args=(self.pk,), queue=CELERY_LONG_TERM_TASK_QUEUE)
 
