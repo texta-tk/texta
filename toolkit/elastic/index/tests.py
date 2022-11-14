f5c8eff4e6658dd31ac50bedfdde04797e8384d7
Marko Kollo <4-mkollo@users.noreply.git.texta.ee>
2022-11-14 09:04:28 +0000

    feature: Added support for clearing read_only blocks on the cluster.
    Changelog: feature

--- toolkit/elastic/index/tests.py
+++ toolkit/elastic/index/tests.py
@@ -14,7 +14,6 @@ class IndexViewsTest(APITestCase):
         # Owner of the project
         cls.user = create_test_user('user', 'my@email.com', 'pw', superuser=True)
 
-
     def setUp(self) -> None:
         self.client.login(username="user", password="pw")
         self.ec = ElasticCore()
@@ -26,15 +25,13 @@ class IndexViewsTest(APITestCase):
             self.ec.es.indices.create(index=index_name, ignore=[400, 404])
             self.ids.append(index.pk)
 
-
     def test_bulk_delete(self):
         url = reverse("v2:index-bulk-delete")
         response = self.client.post(url, data={"ids": self.ids}, format="json")
-        self.assertTrue(response.status_code == status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         for index_name in self.index_names:
             self.assertFalse(self.ec.es.indices.exists(index_name))
         print_output("test_bulk_delete:response.data", response.data)
-
 
     def tearDown(self) -> None:
         indices = Index.objects.filter(pk__in=self.ids)
@@ -43,3 +40,9 @@ class IndexViewsTest(APITestCase):
             indices.delete()
             for index in names:
                 self.ec.delete_index(index=index, ignore=[400, 404])
+
+    def test_that_unlocking_indices_works(self):
+        url = reverse("v2:index-clear-read-only-blocks")
+        response = self.client.post(url)
+        print_output("test_that_unlocking_indices_works:response.data", response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
